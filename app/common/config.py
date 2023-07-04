480fea351b13cd758609624462ec31ab5077a89d
ElizavetaKrutko <117035702+ElizavetaKrutko@users.noreply.github.com>
2023-07-04 17:18:02 +0300

    add data model, migration, linters with pre-commit (#2)
    * add data model, migration, linters with pre-commit
    
    * Edit: gitignore file, enum field, declarative_base
    
    * Delete .idea from git
    
    ---------
    
    Co-authored-by: Elizaveta Krutsko <elizaveta.krutsko@innowise-group.com>

---
+++ app/common/config.py
@@ -0,0 +1,16 @@
+from os.path import dirname, join
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+dotenv_path = join(dirname(__file__), "envs/.env")
+load_dotenv(dotenv_path=dotenv_path)
+
+
+class Settings(BaseSettings):
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+settings = Settings()
